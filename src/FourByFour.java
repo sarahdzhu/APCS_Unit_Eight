5bd19074efe27dbb610d64c1f034b07ce35ade93
sarahdzhu <zhudo@ssfs.org>
2022-02-24 09:38:03 -0500

    two d array

--- src/FourByFour.java
+++ src/FourByFour.java
@@ -1,16 +1,66 @@
+import java.sql.Array;
+
 public class FourByFour {
     public static void main(String [] args) {
 //code to define an initially empty int 2D array, minimum size 4x4
-
+int  [][] s = new int [4][4];
 //code to output all original values
-
+        System.out.println(s[0][0]);
+        System.out.println(s[0][1]);
+        System.out.println(s[0][2]);
+        System.out.println(s[0][3]);
+        System.out.println(s[1][0]);
+        System.out.println(s[1][1]);
+        System.out.println(s[1][2]);
+        System.out.println(s[1][3]);
+        System.out.println(s[2][0]);
+        System.out.println(s[2][1]);
+        System.out.println(s[2][2]);
+        System.out.println(s[2][3]);
+        System.out.println(s[3][0]);
+        System.out.println(s[3][1]);
+        System.out.println(s[3][2]);
+        System.out.println(s[3][3]);
 //code to change half the values to even positive numbers
+        s[0][0]=2;
+        s[0][1]=4;
+        s[0][2]=6;
+        s[0][3]=8;
+        s[1][0]=10;
+        s[1][1]=12;
+        s[1][2]=14;
+        s[1][3]=16;
 
 //and the other half to negative odd numbers
+        s[2][0]=-1;
+        s[2][1]=-3;
+        s[2][2]=-5;
+        s[2][3]=-7;
+        s[3][0]=-9;
+        s[3][1]=-11;
+        s[3][2]=-13;
+        s[3][3]=-15;
 
 //code to output a blank line separating output sets
+        System.out.println();
 
 //code to output all array elements
+        System.out.print(s[0][0]+" ");
+        System.out.print(s[0][1]+" ");
+        System.out.print(s[0][2]+" ");
+        System.out.println(s[0][3]);
+        System.out.print(s[1][0]+" ");
+        System.out.print(s[1][1]+" ");
+        System.out.print(s[1][2]+" ");
+        System.out.println(s[1][3]);
+        System.out.print(s[2][0]+" ");
+        System.out.print(s[2][1]+" ");
+        System.out.print(s[2][2]+" ");
+        System.out.println(s[2][3]);
+        System.out.print(s[3][0]+" ");
+        System.out.print(s[3][1]+" ");
+        System.out.print(s[3][2]+" ");
+        System.out.println(s[3][3]);
     }
 }
 
--- src/ThreeByThree.java
+++ src/ThreeByThree.java
@@ -7,17 +7,36 @@ public class ThreeByThree {
         double [][] list = new double[3][3];
 
 //code to output all original values
-
+        System.out.println(list[0][0]);
+        System.out.println(list[0][1]);
+        System.out.println(list[0][2]);
+        System.out.println(list[1][0]);
+        System.out.println(list[1][1]);
+        System.out.println(list[1][2]);
+        System.out.println(list[2][0]);
+        System.out.println(list[2][1]);
+        System.out.println(list[2][2]);
 
 //code to change five of the values to positive
+        list[0][2]= 9.12;
+        list[2][1]= 7.3;
+        list[2][0]= 5.2;
+        list[1][0]= 4.8;
+        list[1][2]= 3.9;
 
 
 //and the other four to negative
+        list[0][1]= -8.5;
+        list[2][0]= -4.3;
+        list[2][2]=-2.0;
+        list[0][0]= -7.4;
+
 
 
 //code to output a blank line separating output sets
+        System.out.println();
 
-
+        System.out.println(list);
 //code to output all array elements
     }
 }
